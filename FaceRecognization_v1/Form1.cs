1d65fd7b2334ac44dc6f5bad4842b653e7366e58
Hoa Huynh <huuhoa1133@gmail.com>
2018-01-17 21:02:04 +0700

    cmt

--- FaceRecognization_v1/Form1.cs
+++ FaceRecognization_v1/Form1.cs
@@ -20,28 +20,9 @@ namespace FaceRecognization_v1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //var a = new TrainRepo();
-            //a.LoadData();
-            //var labels = a.GetLabel();
-            //var imgs = a.GetTrainImg();
-            //var b = a.DetectFace(imgs[0].Bitmap);
 
             var train = new MLLib();
-            //var bitmap = train.TrainANN();
-            //pictureBox1.Image = bitmap;
-
-
-            //train.TestTraning();
-            //train.Test();
-
-            //for (int i = 0; i < train.imageBindingModel.Count; i++)
-            //{
-            //    pictureBox1.Image = train.imageBindingModel[i].ImageFaceRgb.Bitmap;
-            //    pictureBox2.Image = train.imageBindingModel[i].ImageFaceResizeRgb.Bitmap;
-            //    Thread.Sleep(1000);
-            //}
-
-
+            
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
